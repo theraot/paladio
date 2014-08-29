7cabc1bbea6dbabb437801591542d57f8aaf14d9
= <theraot@gmail.com>
2014-08-29 09:26:09 -0500

    Disabling magic quotes at runtime

--- paladio/core.php
+++ paladio/core.php
@@ -4,3 +4,26 @@
 		header('HTTP/1.0 404 Not Found', true, 404);
 		exit();
 	}
+	
+	//Disabling magic quotes at runtime taken from http://php.net/manual/en/security.magicquotes.disabling.php
+	if (get_magic_quotes_gpc())
+	{
+		$process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+		while (list($key, $val) = each($process))
+		{
+			foreach ($val as $k => $v) 
+			{
+				unset($process[$key][$k]);
+				if (is_array($v))
+				{
+				$process[$key][stripslashes($k)] = $v;
+				$process[] = &$process[$key][stripslashes($k)];
+				}
+				else
+				{
+					$process[$key][stripslashes($k)] = stripslashes($v);
+				}
+			}
+		}
+		unset($process);
+	}
