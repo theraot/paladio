af27a98c57b65b78d7c7fcab228adb048df85cf3
= <theraot@gmail.com>
2014-09-10 17:11:42 -0500

    Extended StringUtility
    Full list of functions:
    EndsWith / StartsWith
    EnsureEnd / EnsureStart
    NeglectEnd / NeglectStart
    TryNeglectEnd / TryNeglectStart

--- paladio/StringUtility.php
+++ paladio/StringUtility.php
@@ -62,6 +62,158 @@
 			}
 		}
 
+		/**
+		 * Returns a new string equal to $string that starts with $start.
+		 *
+		 * If $string starts with $start, the returned string is $string, otherwise $start.$string.
+		 *
+		 * @param $string: the string to verify.
+		 * @param $start: the start to ensure.
+		 *
+		 * @access public
+		 * @return string
+		 */
+		public static function EnsureStart(/*string*/ $string, /*string*/ $start)
+		{
+			if (self::StartsWith($string, $start))
+			{
+				return $string;
+			}
+			else
+			{
+				return $start.$string;
+			}
+		}
+
+		/**
+		 * Returns a new string equal to $string that doesn't end with $ending.
+		 *
+		 * If $string ends with $ending, the returned string is $string without $ending, $string otherwise.
+		 *
+		 * @param $string: the string to verify.
+		 * @param $ending: the ending to neglect.
+		 *
+		 * @access public
+		 * @return string
+		 */
+		public static function NeglectEnd(/*string*/ $string, /*string*/ $ending)
+		{
+			self::TryNeglectEnd($string, $ending, $result);
+			return $result;
+		}
+
+		/**
+		 * Returns a new string equal to $string that doesn't start with $start.
+		 *
+		 * If $string starts with $start, the returned string is $string without $start, $string otherwise.
+		 *
+		 * @param $string: the string to verify.
+		 * @param $start: the start to neglect.
+		 *
+		 * @access public
+		 * @return string
+		 */
+		public static function NeglectStart(/*string*/ $string, /*string*/ $start)
+		{
+			self::TryNeglectStart($string, $start, $result);
+			return $result;
+		}
+
+		/**
+		 * Verifies if a string starts with another string.
+		 *
+		 * Returns true of the string $string starts with $with.
+		 *
+		 * @param $string: the string to verify.
+		 * @param $with: the ending to verify.
+		 *
+		 * @access public
+		 * @return bool
+		 */
+		public static function StartsWith(/*string*/ $string, /*string*/ $with)
+		{
+			if (substr($string, 0, strlen($with)) === $with)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		/**
+		 * Attempts to create a a new string equal to $string that doesn't end with $ending.
+		 *
+		 * If $string ends with $ending, the returns true, false otherwise.
+		 *
+		 * @param $string: the string to verify.
+		 * @param $ending: the ending to neglect.
+		 * @param $result: the new string that was created.
+		 *
+		 * @access public
+		 * @return bool
+		 */
+		public static function TryNeglectEnd(/*string*/ $string, /*string*/ $ending, /*string*/ &$result)
+		{
+			$length = strlen($string);
+			$endLength = strlen($ending);
+			if (substr($string, $length - $endLength) === $ending)
+			{
+				if ($length < $endLength)
+				{
+					$result = '';
+					return true;
+				}
+				else
+				{
+					$result = substr($string, 0, $length - $endLength);
+				}
+				return true;
+			}
+			else
+			{
+				$result = $string;
+				return false;
+			}
+		}
+
+		/**
+		 * Attempts to create a a new string equal to $string that doesn't start with $start.
+		 *
+		 * If $string starts with $start, the returns true, false otherwise.
+		 *
+		 * @param $string: the string to verify.
+		 * @param $ending: the start to neglect.
+		 * @param $result: the new string that was created.
+		 *
+		 * @access public
+		 * @return bool
+		 */
+		public static function TryNeglectStart(/*string*/ $string, /*string*/ $start, /*string*/ &$result)
+		{
+			$startLength = strlen($start);
+			if (substr($string, 0, $startLength) === $start)
+			{
+				$length = strlen($string);
+				if ($length < $startLength)
+				{
+					$result = '';
+					return true;
+				}
+				else
+				{
+					$result = substr($string, $startLength);
+				}
+				return true;
+			}
+			else
+			{
+				$result = $string;
+				return false;
+			}
+		}
+
 		//------------------------------------------------------------
 		// Public (Constructor)
 		//------------------------------------------------------------
